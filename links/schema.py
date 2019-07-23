4a35e17cb210a503d94c9d04e0d64490fee34688
alvezgr <alvezpope@gmail.com>
2019-07-22 21:14:47 -0300

    Mutatios was added to the projects

--- links/schema.py
+++ links/schema.py
@@ -5,7 +5,7 @@ from links.models import Link
 
 
 class LinkType(DjangoObjectType):
-    """DjangoObjectType for GraphQL type"""
+    """DjangoObjectType for GraphQL"""
 
     class Meta:
         model = Link
@@ -20,3 +20,33 @@ class Query(graphene.ObjectType):
         """Function for resolver for Link"""
 
         return Link.objects.all()
+
+
+class CreateLink(graphene.Mutation):
+    """Class for mutation in GraphQL"""
+
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+
+    def mutate(self, info, url, description):
+        """Fuction that will mutate data"""
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+
+class Mutation(graphene.ObjectType):
+    """Class Mutation from graphene ObjectType"""
+    create_link = CreateLink.Field()
